d025b140eeee27001a67304ffc694a526d80ee07
Kuttesch <moritzkuttesch@outlook.de>
2024-12-20 09:55:42 +0100

    added vscode config

---
+++ src/TankYouNext/MissleToe.java
@@ -0,0 +1,11 @@
+package TankYouNext;
+
+import robocode.*;
+
+public class MissleToe extends AdvancedRobot {
+    @Override
+    public void run() {
+        // Example usage
+        turnGunRight(360);  // Rotate gun 360 degrees
+    }
+}
