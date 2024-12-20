cd21e632a9862e2d4129d0a7ad1d6a1317119b94
Kuttesch <moritzkuttesch@outlook.de>
2024-12-20 10:21:54 +0100

    added colors

--- src/TankYouNext/MissleToe.java
+++ src/TankYouNext/MissleToe.java
@@ -1,11 +1,25 @@
 package TankYouNext;
 
 import robocode.*;
+import java.awt.Color;
 
 public class MissleToe extends AdvancedRobot {
     @Override
     public void run() {
-        // Example usage
-        turnGunRight(360);  // Rotate gun 360 degrees
+        while (true) {
+            colorize();
+        }
+    }
+
+    public void colorize() {
+        setBodyColor(calculateRandomColor());
+    }
+
+    public Color calculateRandomColor() {
+        int red = (int) (Math.random() * 256);
+        int green = (int) (Math.random() * 256);
+        int blue = (int) (Math.random() * 256);
+
+        return new Color(red, green, blue);
     }
 }
